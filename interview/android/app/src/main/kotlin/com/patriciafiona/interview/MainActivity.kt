823484f30d657460b9bcda988e5d12cc2608362d
Patricia Fiona <32255348+patriciafiona@users.noreply.github.com>
2022-12-07 08:28:52 +0700

    Delete interview directory

--- interview/android/app/src/main/kotlin/com/patriciafiona/interview/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.patriciafiona.interview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- interview/lib/model/CourseEntity.dart
+++
@@ -1,8 +0,0 @@
-
-class CourseEntity{
-  var name;
-  var desc;
-  var lecturer;
-  var periode;
-  CourseEntity({this.name, this.desc, this.lecturer, this.periode});
-}
