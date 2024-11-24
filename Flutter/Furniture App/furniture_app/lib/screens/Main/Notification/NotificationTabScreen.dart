ffef3c99f377dbf1b220f4134114ad67479a12b3
Patricia Fiona <patriciafiona3@gmail.com>
2024-11-24 12:07:09 +0700

    [Furniture] Finish adding empty notification page

--- Flutter/Furniture App/furniture_app/lib/screens/Main/Notification/NotificationTabScreen.dart
+++ Flutter/Furniture App/furniture_app/lib/screens/Main/Notification/NotificationTabScreen.dart
@@ -10,6 +10,45 @@ class NotificationTabScreen extends StatefulWidget {
 class _NotificationTabScreenState extends State<NotificationTabScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenWidth = MediaQuery.sizeOf(context).width;
+    double screenHeight = MediaQuery.sizeOf(context).height;
+
+    return Container(
+      width: screenWidth,
+      height: screenHeight,
+      padding: const EdgeInsets.only(
+        top: 36, left: 16, right: 16, bottom: 24
+      ),
+      child: Column(
+        children: [
+          const Spacer(),
+
+          Image.asset(
+            "assets/gif/empty_notif.gif",
+            width: screenWidth * 0.5,
+          ),
+          const Text(
+            "No Notification Yet",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontFamily: "Lufga",
+                fontWeight: FontWeight.w600
+            ),
+          ),
+          const Text(
+            "You have no notification right now.\nCome back later",
+            style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+                fontFamily: "Lufga",
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          const Spacer(),
+        ],
+      ),
+    );
   }
 }
