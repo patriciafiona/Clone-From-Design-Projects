3e57b539f2c9b73a20b8412f4e3d21e801ddcbf3
Patricia Fiona <patriciafiona3@gmail.com>
2025-04-23 09:58:41 +0700

    [Travel App] Init based project

---
+++ iOS app with SwiftUI/Travel App/Travel App/Travel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AppApp.swift
+//  Travel App
+//
+//  Created by FIONA Patricia on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Travel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS app with SwiftUI/Travel App/Travel AppTests/Travel_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AppTests.swift
+//  Travel AppTests
+//
+//  Created by FIONA Patricia on 23/04/25.
+//
+
+import Testing
+@testable import Travel_App
+
+struct Travel_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
